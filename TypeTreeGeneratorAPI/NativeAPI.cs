f560747b2f9477d122f7254104d099bc411e5b40
Rudolf Kolbe <rkolbe96@gmail.com>
2025-02-26 23:47:09 +0100

    NativeAPI - add free functions and remove useless unsafe

--- TypeTreeGeneratorAPI/NativeAPI.cs
+++ TypeTreeGeneratorAPI/NativeAPI.cs
@@ -1,11 +1,12 @@
-﻿using System.Runtime.InteropServices;
+﻿using AssetStudio;
+using System.Runtime.InteropServices;
 
 namespace TypeTreeGeneratorAPI
 {
     public static class NativeAPI
     {
         [UnmanagedCallersOnly(EntryPoint = "TypeTreeGenerator_init")]
-        unsafe public static IntPtr TypeTreeGenerator_create(IntPtr unityVersionPtr)
+        public static IntPtr TypeTreeGenerator_create(IntPtr unityVersionPtr)
         {
             string? unityVersion = Marshal.PtrToStringUTF8(unityVersionPtr);
             if (unityVersion == null)
@@ -103,7 +104,7 @@ namespace TypeTreeGeneratorAPI
         }
 
         [UnmanagedCallersOnly(EntryPoint = "TypeTreeGenerator_generateTreeNodesJson")]
-        unsafe public static int TypeTreeGenerator_generateTypeTreeNodesJson(IntPtr typeTreeGeneratorPtr, IntPtr assemblyNamePtr, IntPtr fullNamePtr, IntPtr jsonAddr, IntPtr jsonLength)
+        public static int TypeTreeGenerator_generateTypeTreeNodesJson(IntPtr typeTreeGeneratorPtr, IntPtr assemblyNamePtr, IntPtr fullNamePtr, IntPtr jsonAddr, IntPtr jsonLength)
         {
             string? assemblyName = Marshal.PtrToStringUTF8(assemblyNamePtr);
             string? fullName = Marshal.PtrToStringUTF8(fullNamePtr);
@@ -143,8 +144,8 @@ namespace TypeTreeGeneratorAPI
         }
 
         [UnmanagedCallersOnly(EntryPoint = "TypeTreeGenerator_generateTreeNodesRaw")]
-        unsafe public static int TypeTreeGenerator_generateTypeTreeNodesRaw(IntPtr typeTreeGeneratorPtr, IntPtr assemblyNamePtr, IntPtr fullNamePtr, IntPtr arrAddrPtr, IntPtr arrLengthPtr)
-        {            
+        public static int TypeTreeGenerator_generateTypeTreeNodesRaw(IntPtr typeTreeGeneratorPtr, IntPtr assemblyNamePtr, IntPtr fullNamePtr, IntPtr arrAddrPtr, IntPtr arrLengthPtr)
+        {
             string? assemblyName = Marshal.PtrToStringUTF8(assemblyNamePtr);
             string? fullName = Marshal.PtrToStringUTF8(fullNamePtr);
 
@@ -154,7 +155,7 @@ namespace TypeTreeGeneratorAPI
             }
             try
             {
-                var typeTreeGenerator = (TypeTreeGenerator)GCHandle.FromIntPtr(typeTreeGeneratorPtr).Target!;                
+                var typeTreeGenerator = (TypeTreeGenerator)GCHandle.FromIntPtr(typeTreeGeneratorPtr).Target!;
                 var typeTreeNodes = typeTreeGenerator.GenerateTreeNodes(assemblyName, fullName);
                 if (typeTreeNodes == null)
                 {
@@ -173,10 +174,25 @@ namespace TypeTreeGeneratorAPI
             }
         }
 
+        [UnmanagedCallersOnly(EntryPoint = "TypeTreeGenerator_freeTreeNodesRaw")]
+        public static int TypeTreeGenerator_freeTreeNodesRaw(IntPtr arrAddr, int arrLength)
+        {
+            if (arrAddr == IntPtr.Zero)
+            {
+                return -1;
+            }
+            for (int i = 0; i < arrLength; i++)
+            {
+                Marshal.DestroyStructure<TypeTreeNode>(arrAddr + i * Marshal.SizeOf<TypeTreeNode>());
+            }
+            Marshal.FreeCoTaskMem(arrAddr);
+            return 0;
+        }
+
         [UnmanagedCallersOnly(EntryPoint = "TypeTreeGenerator_getMonoBehaviorDefinitions")]
-        unsafe public static int TypeTreeGenerator_getMonoBehaviorDefinitions(IntPtr typeTreeGeneratorPtr, IntPtr arrAddrPtr, IntPtr arrLengthPtr)
-        {            
-            if (typeTreeGeneratorPtr == IntPtr.Zero )
+        public static int TypeTreeGenerator_getMonoBehaviorDefinitions(IntPtr typeTreeGeneratorPtr, IntPtr arrAddrPtr, IntPtr arrLengthPtr)
+        {
+            if (typeTreeGeneratorPtr == IntPtr.Zero)
             {
                 return -1;
             }
@@ -187,7 +203,7 @@ namespace TypeTreeGeneratorAPI
                 var typeNames = typeTreeGenerator.GetMonoBehaviourDefinitions();
 
                 var arrayLength = typeNames.Count;
-                var arrayPtr = Marshal.AllocCoTaskMem(Marshal.SizeOf<IntPtr>() * arrayLength * 2);                
+                var arrayPtr = Marshal.AllocCoTaskMem(Marshal.SizeOf<IntPtr>() * arrayLength * 2);
                 for (int i = 0; i < arrayLength; i++)
                 {
                     string module = typeNames[i].Module.Name;
@@ -195,7 +211,7 @@ namespace TypeTreeGeneratorAPI
                     Marshal.WriteIntPtr(arrayPtr, (i * 2) * Marshal.SizeOf<IntPtr>(), Marshal.StringToCoTaskMemUTF8(module));
                     Marshal.WriteIntPtr(arrayPtr, (i * 2 + 1) * Marshal.SizeOf<IntPtr>(), Marshal.StringToCoTaskMemUTF8(fullName));
                 }
-            
+
                 Marshal.WriteIntPtr(arrAddrPtr, arrayPtr);
                 Marshal.WriteInt32(arrLengthPtr, arrayLength);
 
@@ -206,6 +222,26 @@ namespace TypeTreeGeneratorAPI
                 return -1;
             }
         }
+
+
+        [UnmanagedCallersOnly(EntryPoint = "TypeTreeGenerator_freeMonoBehaviorDefinitions")]
+        public static int TypeTreeGenerator_freeMonoBehaviorDefinitions(IntPtr arrAddr, int arrLength)
+        {
+            if (arrAddr == IntPtr.Zero)
+            {
+                return -1;
+            }
+            for (int i = 0; i < arrLength; i++)
+            {
+                var modulePtr = Marshal.ReadIntPtr(arrAddr, i * IntPtr.Size * 2);
+                var fullNamePtr = Marshal.ReadIntPtr(arrAddr, i * IntPtr.Size * 2 + IntPtr.Size);
+                Marshal.FreeCoTaskMem(modulePtr);
+                Marshal.FreeCoTaskMem(fullNamePtr);
+            }
+            Marshal.FreeCoTaskMem(arrAddr);
+            return 0;
+        }
+
         [UnmanagedCallersOnly(EntryPoint = "FreeCoTaskMem")]
         public static void FreeCoTaskMem(IntPtr ptr)
         {
