7756296a3e910df1ca9578932e67d7c510ed061d
Rudolf Kolbe <rkolbe96@gmail.com>
2025-06-06 13:22:45 +0200

    chore(python): bump version to 0.0.6

--- bindings/python/TypeTreeGeneratorAPI/__init__.py
+++ bindings/python/TypeTreeGeneratorAPI/__init__.py
@@ -1,6 +1,6 @@
 from .TypeTreeGenerator import TypeTreeBackend, TypeTreeGenerator, TypeTreeNode, TypeTreeNodeNative
 
-version = __version__ = "0.0.5"
+version = __version__ = "0.0.6"
 
 __all__ = [
     "__version__",
