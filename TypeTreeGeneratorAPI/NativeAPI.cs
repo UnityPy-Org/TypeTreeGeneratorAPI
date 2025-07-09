e9cc8a1eca9d63c397dc72be42566e40ed1af2ba
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-07-09 10:36:22 +0200

    native api: on nonexistent type, return null instead of error

--- TypeTreeGeneratorAPI/NativeAPI.cs
+++ TypeTreeGeneratorAPI/NativeAPI.cs
@@ -148,7 +148,8 @@ namespace TypeTreeGeneratorAPI
                 var typeTreeNodes = handle.Instance.GenerateTreeNodes(assemblyName, fullName);
                 if (typeTreeNodes == null)
                 {
-                    return -1;
+                    Marshal.WriteIntPtr(jsonAddr, IntPtr.Zero);
+                    return 0;
                 }
                 var json = TypeTreeNodeSerializer.ToJson(typeTreeNodes!);
                 Marshal.WriteIntPtr(jsonAddr, Marshal.StringToCoTaskMemUTF8(json));
@@ -177,7 +178,9 @@ namespace TypeTreeGeneratorAPI
                 var typeTreeNodes = handle.Instance.GenerateTreeNodes(assemblyName, fullName);
                 if (typeTreeNodes == null)
                 {
-                    return -1;
+                    Marshal.WriteIntPtr(arrAddrPtr, IntPtr.Zero);
+                    Marshal.WriteInt32(arrLengthPtr, 0);
+                    return 0;
                 }
                 var (arrayPtr, arrayLength) = TypeTreeNodeSerializer.ToRaw(typeTreeNodes!);
 
