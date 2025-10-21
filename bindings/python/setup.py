8de1a456b17604ba061fe5c23a4d36f677aa1e25
Rudolf Kolbe <rkolbe96@gmail.com>
2025-10-21 16:23:07 +0200

    refreactor(py bindings): use modern setuptools, allow build_ext (copy files), disable purelib

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -3,11 +3,9 @@ import shutil
 from typing import Tuple
 
 from setuptools import setup
-
-try:
-    from setuptools.command.bdist_wheel import bdist_wheel
-except ImportError:
-    from wheel.bdist_wheel import bdist_wheel  # type: ignore
+from setuptools.command.bdist_wheel import bdist_wheel
+from setuptools.command.build_ext import build_ext
+from setuptools.dist import Distribution
 
 
 def copy_binary_files(net_rid: str):
@@ -30,6 +28,18 @@ def copy_binary_files(net_rid: str):
             shutil.copy(os.path.join(publish_dir, item), os.path.join(target_dir, item))
 
 
+def copy_binary_files_plat(platform_tag: str):
+    if platform_tag.startswith("win"):
+        net_rid = BDIST_TAG_MAP_WIN[platform_tag]
+    elif platform_tag.startswith("macosx"):
+        net_rid = next((v for k, v in BDIST_TAG_MAP_MAC.items() if platform_tag.endswith(k)))
+    else:
+        net_rid = next((v for k, v in BDIST_TAG_MAP_LINUX.items() if platform_tag.endswith(k)))
+        if "musllinux" in platform_tag:
+            net_rid = net_rid.replace("-", "-musl-")
+    copy_binary_files(net_rid)
+
+
 # see:
 # https://learn.microsoft.com/en-us/dotnet/core/rid-catalog
 BDIST_TAG_MAP_WIN = {
@@ -48,31 +58,37 @@ BDIST_TAG_MAP_LINUX = {
 
 
 class bdist_wheel_abi3(bdist_wheel):
-    def finalize_options(self):
-        super().finalize_options()
-        self.root_is_pure = False
-
     def run(self):
         platform_tag = self.get_tag()[2]
-        if platform_tag.startswith("win"):
-            net_rid = BDIST_TAG_MAP_WIN[platform_tag]
-        elif platform_tag.startswith("macosx"):
-            net_rid = next((v for k, v in BDIST_TAG_MAP_MAC.items() if platform_tag.endswith(k)))
-        else:
-            net_rid = next((v for k, v in BDIST_TAG_MAP_LINUX.items() if platform_tag.endswith(k)))
-            if "musllinux" in platform_tag:
-                net_rid = net_rid.replace("-", "-musl-")
-        copy_binary_files(net_rid)
+        copy_binary_files_plat(platform_tag)
         super().run()
 
     def get_tag(self) -> Tuple[str, str, str]:
+        self.root_is_pure = False
         python, abi, plat = super().get_tag()
+        self.root_is_pure = True
         if python.startswith("cp"):
             # on CPython, our wheels are abi3 and compatible back to 3.7
             return "cp36", "abi3", plat
         return python, abi, plat
 
 
+class build_ext_c(build_ext):
+    def run(self):
+        platform_tag = self.plat_name.replace("-", "_")
+        copy_binary_files_plat(platform_tag)
+        super().run()
+
+
+class UnpureDistribution(Distribution):
+    """Distribution which is not marked as pure, so that wheels are built as platform-specific"""
+
+    def is_pure(self) -> bool:
+        return False
+
+
 setup(
-    cmdclass={"bdist_wheel": bdist_wheel_abi3},
+    cmdclass={"bdist_wheel": bdist_wheel_abi3, "build_ext": build_ext_c},
+    distclass=UnpureDistribution,
+    zip_safe=False,
 )
